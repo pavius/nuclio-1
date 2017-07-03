625d41e9ec112b8877ed14ffb55c65a861a72a96
Yaron Haviv <yaronh@iguaz.io>
2017-07-03 15:59:44 +0300

    initial

---
+++ cmd/controller/controller.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+"flag"
+"fmt"
+"os"
+
+"github.com/nuclio/nuclio/cmd/processor/app"
+)
+
+func run() error {
+	configPath := flag.String("config", "", "Path of configuration file")
+	flag.Parse()
+
+	processor, err := app.NewProcessor(*configPath)
+	if err != nil {
+		return err
+	}
+
+	return processor.Start()
+}
+
+func main() {
+
+	if err := run(); err != nil {
+		fmt.Printf("Failed to run controller: %s", err)
+
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
